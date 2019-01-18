2d278087da78a05f8956121c3d7e120c55cb5238
Knight-7 <Knight-7@users.noreply.github.com>
2019-01-19 02:49:43 +0800

    飞船它lei了

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,17 +2,20 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen_width=1200
-    screen_height=800
-    screen = pygame.display.set_mode((screen_width,screen_height),0,32)
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
 
-    # 设置背景色
-    bg_color=(230,230,230)
+    # 创建一艘飞船
+    ship=Ship(screen)
 
     # 开始游戏的主循环
     while True:
@@ -21,9 +24,10 @@ def run_game():
         for even in pygame.event.get():
             if even.type == pygame.QUIT:
                 sys.exit()
-        
+
         # 每次循环时都重绘屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
