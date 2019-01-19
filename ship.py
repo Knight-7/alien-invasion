705044c42d632c1d5555d3d864004f7f8ffaed49
Knight-7 <Knight-7@users.noreply.github.com>
2019-01-19 14:13:07 +0800

    飞船的移动

--- ship.py
+++ ship.py
@@ -1,21 +1,38 @@
 import pygame
 
+
 class Ship():
     """飞船的类"""
 
-    def __init__(self,screen):
+    def __init__(self,ai_settings, screen):
         """初始化飞船并设置其初始位置"""
-        self.screen=screen
+        self.screen = screen
+        self.ai_settings=ai_settings
 
         # 加载飞船图像并获取其外接矩形
         self.image = pygame.image.load('alien_invasion/images/ship.bmp')
         self.rect = self.image.get_rect()
-        self.screen_rect = screen.get_rect()
+        self.screen_rect = self.screen.get_rect()
+
+        # 设置移动标志
+        self.moving_right = False
+        self.moving_left = False
 
         # 将每艘飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # 将飞船的center属性中存储小数值
+        self.center = float(self.rect.centerx)
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
+
     def blitme(self):
         """在指定位置绘制飞船"""
-        self.screen.blit(self.image,self.rect)
+        self.screen.blit(self.image, self.rect)
